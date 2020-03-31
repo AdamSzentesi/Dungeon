469296433dc81fce56ce8015948d4ac853e18510
Adam Szentesi <adam.szentesi@gmail.com>
2020-03-31 10:33:29 +0200

    Simple restart on death

---
+++ Assets/Scripts/GUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUI : MonoBehaviour
+{
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
